1f5c529772402978f68e4ffe8ee9f6e838b1b5aa
stephen <47056935+stephensund@users.noreply.github.com>
2021-03-17 14:53:42 +0800

    update

--- scripts/lean-armv7.sh
+++ scripts/lean-armv7.sh
@@ -29,19 +29,19 @@ find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray package/lean/v2ray
 
 # 为lean源添加sub-web & subconverter
+rm -rf feeds/packages/lang/node
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/sub-web package/sub-web
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/subconverter package/subconverter
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/duktape package/duktape
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/jpcre2 package/jpcre2
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/rapidjson package/rapidjson
+svn co https://github.com/immortalwrt/packages/trunk/lang/node package/node
 svn co https://github.com/immortalwrt/packages/trunk/lang/node-yarn package/node-yarn
 
-# Add Jerrykuku's packages(vssr/jd-daily/argon theme)
+# Add Jerrykuku's packages
 rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/lua-maxminddb package/jerrykuku/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-argon-config package/jerrykuku/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/jerrykuku/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
 
